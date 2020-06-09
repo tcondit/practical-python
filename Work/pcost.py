5c75c640a84428e6236e4f2a8ca52ab87018d607
Tim Condit <tcondit@gmail.com>
2020-06-09 10:17:35 -0700

    Add exercise 1.27 (reading a data file)

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,11 @@
 # pcost.py
 #
 # Exercise 1.27
+
+pcost = 0
+with open("Data/portfolio.csv", "rt") as f:
+    header = next(f)
+    for line in f:
+        line = line.split(",")
+        pcost = pcost + int(line[1]) * float(line[2])
+print(f"portfolio cost: {pcost}")
