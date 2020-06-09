28a861c74f02f672d478f73bd47c6ea43532deeb
Tim Condit <tcondit@gmail.com>
2020-06-09 10:38:52 -0700

    Update for exercise 1.30 (turn a script into a function)

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,11 +1,17 @@
 # pcost.py
 #
-# Exercise 1.27
+# Exercise 1.27 and 1.30
 
-pcost = 0
-with open("Data/portfolio.csv", "rt") as f:
-    header = next(f)
-    for line in f:
-        line = line.split(",")
-        pcost = pcost + int(line[1]) * float(line[2])
+
+def portfolio_cost(filename):
+    pcost = 0
+    with open(filename, "rt") as f:
+        header = next(f)
+        for line in f:
+            line = line.split(",")
+            pcost = pcost + int(line[1]) * float(line[2])
+    return pcost
+
+
+pcost = portfolio_cost("Data/portfolio.csv")
 print(f"portfolio cost: {pcost}")
